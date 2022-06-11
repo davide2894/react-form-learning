6b2bc230c4e57916c8db69a5ad31c8c4496c7d5d
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2022-06-11 18:37:16 +0200

    add and study checkbox field in react forms

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -4,19 +4,29 @@ import { useState } from "react";
 export default function Form(){
     const [formData, setFormData] = useState({
         firstName: "",
-        secondName: ""
+        lastName: "",
+        email: "",
+        textArea: "",
+        isFriendly: false
     });
     
     function handleChange(event) {
+        const {name, value, type, checked} = event.target;
         setFormData(prevFormData => {
             return {
                 ...prevFormData,
-                [event.target.name]: event.target.value 
+                [name]: type === "checkbox" ? checked : value
             }
         })
+        
         console.log(formData);
     }
     
+    /**
+     * Challenge: Add a textarea for "comments" to the form
+     * Make sure to update state when it changes.
+     */
+
     return (
         <form>
             <input
@@ -24,13 +34,36 @@ export default function Form(){
                 placeholder="First Name"
                 onChange={handleChange}
                 name="firstName"
+                value={formData.firstName}
             />
             <input
                 type="text"
                 placeholder="Last Name"
                 onChange={handleChange}
                 name="lastName"
+                value={formData.lastName}
             />
+            <input 
+                type="email" 
+                placeholder="Email"
+                onChange={handleChange}
+                name="email"
+                value={formData.email}
+            />
+            <textarea
+                placeholder="Text Area"
+                onChange={handleChange}
+                name="textArea"
+                value={formData.textArea}
+            />
+            <input
+                onChange={handleChange}
+                id="checkbox" 
+                type="checkbox"
+                name="isFriendly"
+                value={formData.isFriendly}
+            />
+            <label htmlFor="#checkbox">Are you friendly?</label>
         </form>
     )
 }
