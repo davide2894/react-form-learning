14f3198c38a81c832f1c9ae191dbcc4f404e0763
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2022-06-11 17:51:56 +0200

    track multiple form input state change

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,9 +1,39 @@
 import "./Form.scss";
+import { useState } from "react";
 
 export default function Form(){
+    const [formData, setFormData] = useState({
+        firstName: "",
+        secondName: ""
+    });
+    
+    function handleChange(event) {
+        /**
+         * Challenge: update the firstName state on every keystroke
+         */
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value 
+            }
+        })
+        console.log(formData);
+    }
+    
     return (
         <form>
-            
+            <input
+                type="text"
+                placeholder="First Name"
+                onChange={handleChange}
+                name="firstName"
+            />
+            <input
+                type="text"
+                placeholder="Last Name"
+                onChange={handleChange}
+                name="lastName"
+            />
         </form>
     )
 }
